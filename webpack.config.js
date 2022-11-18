c16c333fdd681c2c401ed1dcd603a695e1d78488
robertocannella <robertocannella@gmail.com>
2022-11-17 22:35:47 -0500

    Add typescript typescript ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    entry: "./src/index.ts",
+    resolve: {
+        extensions: ['js','.ts','tsx']
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
     plugins: [
-        new HtmlWebPackPlugin({     // generates <script> tags inside html file
+        new HWebPackPlugin({     // generates <script> tags inside html file
             template: './src/index.html',   // location of html file
             filename: 'index.html'          // filename to use
         })
